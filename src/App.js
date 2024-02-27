fba2223ef7ca416841c6dc351275b03edef4ed40
Roopam <roopamr2411@gmail.com>
2024-02-27 20:35:46 +0530

    fix responsive problem

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/SignUp/SignUp";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Home from "./Pages/Home/Home";
+// import Home from "./Pages/Home/Home";
 import SignIn from "./Pages/SignIn/SignIn";
 import "./index.css";
+import NewHome from '../src/Pages/NewHome/NewHome'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<NewHome />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={< SignIn/>} />
       </Routes>
---
+++ src/Pages/NewHome/NewHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../../Assets/goalsetter.png'
+
+const NewHome = () => {
+  return (
+    <nav>
+      <img src={img} alt="" />
+    </nav>
+  )
+}
+
+export default NewHome
